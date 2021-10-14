842b93c811044fa9b8e3ea4de31e1dbaaf84ea99
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-13 21:40:41 -0700

    anit sql-injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+  const cohortName = process.argv[2];
+  const limit = process.argv[3] || 5;
+  // Store all potentially malicious values in an array.
+  const values = [`%${cohortName}%`, limit];
+
+  pool.query(queryString, values);
+
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
